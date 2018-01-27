715c26cb1bfb9ea27e4f3afb9c7fd44d7f93ff07
RADION Project <31939627+amakary@users.noreply.github.com>
2018-01-27 12:11:00 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,366 @@
+<?php require_once("C:/Inetpub/vhosts/radion.work/httpdocs/radion/radion/slpw/sitelokregister.php"); ?>
+
+<!DOCTYPE html>
+<html class="no-js" lang="en-US">
+<head>
+
+<!-- META DATA -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+<!-- Title -->
+<title>RadiON Private Seed Funding</title>
+
+
+<!-- CSS Global Compulsory -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css" >
+
+<!-- CSS Implementing Plugins -->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+<link rel="stylesheet" type="text/css" href="css/ionicons.min.css" />
+<link rel="stylesheet" type="text/css" href="css/animate.min.css" />
+<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css" />
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css" />
+<link rel="stylesheet" type="text/css" href="css/vegas.min.css" />
+<link rel="stylesheet" type="text/css" href="css/menu.css" />
+<link rel="stylesheet" type="text/css" href="css/sidebar.css" />
+
+<!-- Fonts -->	
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,700italic,400,300,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,900' rel='stylesheet' type='text/css'>
+
+
+<!-- JS -->
+<script type="text/javascript" src="js/modernizr.js"></script>
+<script type="text/javascript" src="js/snap.svg-min.js"></script>
+
+<!-- Custom Script powered by Frans -->
+<script type="text/javascript" src="js/backgroundChange.js"></script>
+<!-- Custom Script powered by Frans -->
+<style>
+.profile-img
+{
+    width: 110px;
+    height: 110px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+}
+.form-signin input[type="text"]
+{
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.form-signin input[type="password"]
+{
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+#container {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    visibility:hidden;
+    display:none;
+    background-color: rgba(22,22,22,0.5);
+}
+
+#container:target {
+    visibility: visible;
+    display: block;
+}
+</style>
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script src="http://webnextbd.net/demo/js/respond.min.js"></script>
+<![endif]-->
+
+<!--[if lt IE 11]>
+<link rel="stylesheet" type="text/css" href="http://webnextbd.net/demo/css/ie.css">
+<![endif]-->
+
+<div class="container">
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog" align="center" style="padding-top:5%">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content" style="max-width:350px; padding-bottom:20px;">
+
+
+            <h3 align="center" style="color:#333; padding-top:30px; padding-bottom:20px;"><strong>LOGIN</strong></h3>
+
+                <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                    alt="">
+                <form class="form-signin" style="padding:30px;">
+                <input type="text" class="form-control" placeholder="Email" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <label class="checkbox" style="color:#333; padding-left:2%;">
+                <input type="checkbox" value="remember-me">
+                    Remember me
+
+                <a href="#" style="color:#333; padding-left:70px;">Need help?</a>
+				</label>
+				
+                </form>
+            <a href="#" style="color:#333; padding-left:35%;">Create an Account</a>
+
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+
+
+<div class="container">
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog" align="center" style="padding-top:5%">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content" style="max-width:350px; padding-bottom:20px;">
+
+
+            <h3 align="center" style="color:#333; padding-top:30px; padding-bottom:20px;"><strong>LOGIN</strong></h3>
+
+                <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                    alt="">
+                <form class="form-signin" style="padding:30px;">
+                <input type="text" class="form-control" placeholder="Email" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <label class="checkbox" style="color:#333; padding-left:2%;">
+                <input type="checkbox" value="remember-me">
+                    Remember me
+
+                <a href="#" style="color:#333; padding-left:70px;">Need help?</a>
+				</label>
+				
+                </form>
+            <a href="#" style="color:#333; padding-left:35%;">Create an Account</a>
+
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+
+
+</head>
+<div align="right" style="padding-right:5%; margin-top:20px;" class="socials-icons animated bounceInUp" data-animation-delay="700">
+<a href="#myModal" id="myBtn" style="padding-right:20px;"><i class="fa fa-fw fa-sign-in"></i> Login</a> | <a href="#" style="padding-right:20px; padding-left:20px;"><i class="fa fa-fw fa-user-plus"></i> Register</a> | <a href="spanish.php" style="padding-right:20px; padding-left:20px;"><i class="fa fa-fw fa-language"></i> Español</a> -
+<a href="#" style="padding-right:15px;padding-left:20px;"><i class="fa fa-fw fa-facebook"></i></a>
+<a href="#" style="padding-right:15px;"><i class="fa fa-fw fa-twitter"></i></a>
+<a href="#" style="padding-right:15px;"><i class="fa fa-fw fa-youtube-square"></i></a>
+</div>
+
+<body class="image-background" >
+<!-- Preloader -->
+<div id="preloader">
+<div id="loading-animation">&nbsp;</div>
+</div>
+<!-- /Preloader -->
+
+<!-- Overlay -->
+<div id="section-home" class="home-section-wrap center">
+<div class="section-overlay"></div>
+</div>
+<!-- /Overlay -->
+
+<!-- container -->
+<div class="containers">
+<!-- menu -->
+<nav id="menu" class="menu">
+<button class="menu__handle"><span>Menu</span></button>
+<div class="menu__inner">
+<ul>
+<li><a href="index.html"><i class="fa fa-fw fa-home"></i><span>Home</span></a></li>
+<li><a href="about.html"><i class="fa fa-fw fa-ticket"></i><span>Membership</span></a></li>
+<li><a href="team.html"><i class="fa fa-fw fa-file-text"></i><span>White Paper</span></a></li>
+<li><a href="https://ropsten.etherscan.io/address/0x96c0d7e7bdd254129513c3dc449aa781e45fef82#readContract"><i class="fa fa-fw fa-code"></i><span>Smart Contract</span></a></li>
+<li><a href="about.html"><i class="fa fa-fw fa-user"></i><span>Founder</span></a></li>
+<li><a href="contact.html"><i class="fa fa-fw fa-envelope"></i><span>Contact</span></a></li>
+</ul>
+</div>
+<div class="morph-shape" data-morph-open="M300-10c0,0,295,164,295,410c0,232-295,410-295,410" data-morph-close="M300-10C300-10,5,154,5,400c0,232,295,410,295,410">
+<svg width="100%" height="100%" viewBox="0 0 600 800" preserveAspectRatio="none">
+<path fill="none" d="M300-10c0,0,0,164,0,410c0,232,0,410,0,410"/>
+</svg>
+</div>
+</nav><!-- /menu -->
+<!-- main -->
+<div class="main">
+<!-- header -->
+<header class="header-site">
+<div class="logo-site animated bounceInDown" data-animation-delay="700">
+<img class="img-responsive" src="images/main-logo.png" data-at2x="images/logo.png" alt="Logo" />
+</div>
+</header>
+<!-- /header -->
+
+<!-- section -->
+                
+<section class="section fullscreen site-main">
+<div class="container-section">
+<div class="container-table">
+<div class="content-table">
+<!-- bt-carousel -->
+<div class="bt-carousel">
+
+<!-- home section -->
+<div class="item-slide show">
+<!-- container -->
+<div class="container">
+<div class="row">
+<div class="col-md-10 col-md-offset-1">
+
+<!-- Title section -->
+<h3 align="center" style="font-family:Impact;">PRIVATE SEED FUNDING</h3>
+<p style="color:#fff; font-size:12px;">M E M B E R S H I P&nbsp;&nbsp;&nbsp;S A L E S&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;PRIOR TO ICO</p>
+
+<!-- content slide -->
+<div class="content-slide">
+<!-- Clock Countdown -->
+<div id="clock-countdown" class="animated bounceInDown" data-animation-delay="700"></div>
+<div align="center" class="color-white top-margin animated bounceInDown" 
+style="font-family: Arial; 
+background-color:#000; 
+border-radius:35px; 
+border:1px solid #515a5a; 
+opacity:0.6; 
+padding-top:15px; 
+padding-bottom:15px;" data-animation-delay="700" >
+<p align="center" style="font-size:24px; padding-top:10px;"><strong>1.3 Million Membership Units for Sale!</strong></p>
+<span style="color:#e6e6e6;">ETH = <span id="token_price"></span> USD<span style="color:#e6e6e6; padding-left:30px;">RADIO = $0.85 USD</span><span style="color:#e6e6e6; padding-left:30px;">Members = 0</span>
+<div align="right" style="padding-right:5%; margin-top:10px;" class="socials-icons animated bounceInUp" data-animation-delay="700">We also Accept:
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-credit-card"></i></a>
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-cc-visa"></i></a>
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-cc-mastercard"></i></a>
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-paypal"></i></a>
+</div></div>
+
+
+<!-- copyright -->
+<footer class="copy-right animated bounceInUp" data-animation-delay="700">
+<br>
+<br>
+<br>
+<div class="copyright">
+© 2018 RadiON - All Rights Reserved.
+</div>
+<p style="margin-top:-30px;">Project Conducted by G Rehl Software Development LLC.</p>
+<hr>
+<a href="#" style="padding-right:30px;">We are Hiring</a> | <a href="private-seed-funding.html" style="padding-left:30px; padding-right:30px;">Private Seed Fundings</a> | <a href="#" style="padding-left:30px;">Business Development</a>
+</footer><!-- /copyright -->
+
+</div><!-- /content slide -->
+</div>
+</div><!-- /row -->
+</div><!-- /container -->
+</div>
+<!-- /home section -->
+
+</div><!-- /bt-carousel -->
+</div>
+</div>
+</div>
+</section><!-- /section -->  
+</div><!-- /main -->
+</div><!-- /container -->        
+
+<!-- JS -->
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/retina.min.js"></script>
+<script type="text/javascript" src="js/jquery.backstretch.min.js"></script>
+<script type="text/javascript" src="js/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="js/jquery.parallaxify.min.js"></script>
+<script type="text/javascript" src="js/jquery.particleground.min.js"></script>
+<script type="text/javascript" src="js/vegas.min.js"></script>
+<script type="text/javascript" src="js/trianglify.min.js"></script>
+<script type="text/javascript" src="js/jquery.mb.YTPlayer.js"></script>
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/jquery.appear.js"></script>
+<script type="text/javascript" src="js/classie.js"></script>
+<script type="text/javascript" src="js/sidebar.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+
+
+<!-- API added by Frans -->
+
+	<script>
+	
+	$(document).ready(function(){
+		
+		 $.ajax(
+            {
+                url: 'https://api.coinmarketcap.com/v1/ticker/ethereum/',
+                type: "GET",
+                success:function(data, textStatus, jqXHR)
+                {
+					
+					
+					
+					var vlolumn_usd=data[0]["24h_volume_usd"];
+					var token_price_raw =data[0].price_usd;
+				
+					var num = new Number(token_price_raw);
+					var token_price=num.toFixed(2);
+					
+					
+					var market_cap_usd = data[0].market_cap_usd;
+					
+					
+					$("#market_cap").html("$"+market_cap_usd);
+					$("#token_price").html("$"+token_price);
+					$("#volume_price").html("$"+vlolumn_usd);
+					
+
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+
+                    console.log("Ajax Failed Error In Saving:" + textStatus);
+
+
+                }
+            });
+			
+		
+		
+	});
+	
+	</script>
+
+<!-- API added by Frans -->
+
+<!-- Modal -->
+<script>
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
+</script>
+<!-- Modal Ends -->
+
+</html>
---
+++ spanish.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en-US">
+<head>
+
+<!-- META DATA -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+<!-- Title -->
+<title>RadiON Private Seed Funding</title>
+
+
+<!-- CSS Global Compulsory -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css" >
+
+<!-- CSS Implementing Plugins -->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+<link rel="stylesheet" type="text/css" href="css/ionicons.min.css" />
+<link rel="stylesheet" type="text/css" href="css/animate.min.css" />
+<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css" />
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css" />
+<link rel="stylesheet" type="text/css" href="css/vegas.min.css" />
+<link rel="stylesheet" type="text/css" href="css/menu.css" />
+<link rel="stylesheet" type="text/css" href="css/sidebar.css" />
+
+<!-- Fonts -->	
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,700italic,400,300,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,900' rel='stylesheet' type='text/css'>
+
+
+<!-- JS -->
+<script type="text/javascript" src="js/modernizr.js"></script>
+<script type="text/javascript" src="js/snap.svg-min.js"></script>
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script src="http://webnextbd.net/demo/js/respond.min.js"></script>
+<![endif]-->
+
+<!--[if lt IE 11]>
+<link rel="stylesheet" type="text/css" href="http://webnextbd.net/demo/css/ie.css">
+<![endif]-->
+
+</head>
+<div align="right" style="padding-right:5%; margin-top:20px;" class="socials-icons animated bounceInUp" data-animation-delay="700">
+<a href="#" style="padding-right:20px;"><i class="fa fa-fw fa-sign-in"></i> Iniciar Sesión</a> | <a href="#" style="padding-right:20px; padding-left:20px;"><i class="fa fa-fw fa-user-plus"></i> Registrate</a> | <a href="index.html" style="padding-right:20px; padding-left:20px;"><i class="fa fa-fw fa-language"></i> English</a>-
+<a href="#" style="padding-right:15px;padding-left:20px;"><i class="fa fa-fw fa-facebook"></i></a>
+<a href="#" style="padding-right:15px;"><i class="fa fa-fw fa-twitter"></i></a>
+<a href="#" style="padding-right:15px;"><i class="fa fa-fw fa-linkedin"></i></a>
+</div>
+
+<body class="image-background">
+<!-- Preloader -->
+<div id="preloader">
+<div id="loading-animation">&nbsp;</div>
+</div>
+<!-- /Preloader -->
+
+<!-- Overlay -->
+<div id="section-home" class="home-section-wrap center">
+<div class="section-overlay"></div>
+</div>
+<!-- /Overlay -->
+
+<!-- container -->
+<div class="containers">
+<!-- menu -->
+<nav id="menu" class="menu">
+<button class="menu__handle"><span>Menu</span></button>
+<div class="menu__inner">
+<ul>
+<li><a href="index.html"><i class="fa fa-fw fa-home"></i><span>Inicio</span></a></li>
+<li><a href="about.html"><i class="fa fa-fw fa-info"></i><span>Quienes Somos</span></a></li>
+<li><a href="team.html"><i class="fa fa-fw fa-file-text-o"></i><span>Paper Blanco</span></a></li>
+<li><a href="services.html"><i class="fa fa-fw fa-map-signs"></i><span>Mapa de Ruta</span></a></li>
+<li><a href="portfolio.html"><i class="fa fa-fw fa-flask"></i><span>Lab</span></a></li>
+<li><a href="contact.html"><i class="fa fa-fw fa-envelope"></i><span>Contactanos</span></a></li>
+</ul>
+</div>
+<div class="morph-shape" data-morph-open="M300-10c0,0,295,164,295,410c0,232-295,410-295,410" data-morph-close="M300-10C300-10,5,154,5,400c0,232,295,410,295,410">
+<svg width="100%" height="100%" viewBox="0 0 600 800" preserveAspectRatio="none">
+<path fill="none" d="M300-10c0,0,0,164,0,410c0,232,0,410,0,410"/>
+</svg>
+</div>
+</nav><!-- /menu -->
+<!-- main -->
+<div class="main">
+<!-- header -->
+<header class="header-site">
+<div class="logo-site animated bounceInDown" data-animation-delay="700">
+<img class="img-responsive" src="images/main-logo.png" data-at2x="images/logo.png" alt="Logo" />
+</div>
+</header>
+<!-- /header -->
+
+<!-- section -->
+                
+<section class="section fullscreen site-main">
+<div class="container-section">
+<div class="container-table">
+<div class="content-table">
+<!-- bt-carousel -->
+<div class="bt-carousel">
+
+<!-- home section -->
+<div class="item-slide show">
+<!-- container -->
+<div class="container">
+<div class="row">
+<div class="col-md-10 col-md-offset-1">
+
+<!-- Title section -->
+<h3 align="center" style="font-family:Impact;">RECOLECTANDO FONDOS SEMILLA</h3>
+<p style="color:#fff; font-size:17px;">V E N T A&nbsp;&nbsp;&nbsp;P R I V A D A&nbsp;&nbsp;&nbsp;D E&nbsp;&nbsp;&nbsp;M E M B R E S I A</p>
+
+<!-- content slide -->
+<div class="content-slide">
+<!-- Clock Countdown -->
+<div id="clock-countdown" class="animated bounceInDown" data-animation-delay="700"></div>
+<div align="center" class="color-white top-margin animated bounceInDown" 
+style="font-family: Arial; 
+background-color:#000; 
+border-radius:35px; 
+border:1px solid #515a5a; 
+opacity:0.6; 
+padding-top:15px; 
+padding-bottom:15px;" data-animation-delay="700" >
+<p align="center" style="font-size:24px; padding-top:10px;"><strong>Solo 1.3 Millon de Membresias a la Venta!</strong></p>
+ETH WALLET:&nbsp;&nbsp;&nbsp;0x310EC7780d07Ae2caafC01545E5b824275167b54
+<div align="right" style="padding-right:5%; margin-top:10px;" class="socials-icons animated bounceInUp" data-animation-delay="700">Aceptamos:
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-credit-card"></i></a>
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-cc-visa"></i></a>
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-cc-mastercard"></i></a>
+<a href="#" style="padding-right:5px;"><i class="fa fa-fw fa-paypal"></i></a>
+</div></div>
+
+
+<!-- copyright -->
+<footer class="copy-right animated bounceInUp" data-animation-delay="700">
+<br>
+<br>
+<br>
+<div class="copyright">
+© 2018 RadiON - Todos los Derechos Reservados.
+</div>
+<p style="margin-top:-30px;">Este projecto es conducido por G Rehl Software Development LLC.</p>
+</footer><!-- /copyright -->
+
+</div><!-- /content slide -->
+</div>
+</div><!-- /row -->
+</div><!-- /container -->
+</div>
+<!-- /home section -->
+
+</div><!-- /bt-carousel -->
+</div>
+</div>
+</div>
+</section><!-- /section -->  
+</div><!-- /main -->
+</div><!-- /container -->        
+
+<!-- JS -->
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/retina.min.js"></script>
+<script type="text/javascript" src="js/jquery.backstretch.min.js"></script>
+<script type="text/javascript" src="js/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="js/jquery.parallaxify.min.js"></script>
+<script type="text/javascript" src="js/jquery.particleground.min.js"></script>
+<script type="text/javascript" src="js/vegas.min.js"></script>
+<script type="text/javascript" src="js/trianglify.min.js"></script>
+<script type="text/javascript" src="js/jquery.mb.YTPlayer.js"></script>
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/jquery.appear.js"></script>
+<script type="text/javascript" src="js/classie.js"></script>
+<script type="text/javascript" src="js/sidebar.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+</html>
